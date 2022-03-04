926659f2789be7ab850902cecc52409c421efe9e
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 17:27:26 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -2,11 +2,11 @@
 
 import os
 
-from plugins.config import Config
 
-from plugins.button import youtube_dl_call_back
-from plugins.link import ddl_call_back
-from plugins.main import Translation
+
+from plugins.youtube_dl_button import youtube_dl_call_back
+from plugins.dl_button import ddl_call_back
+from Translation import Translation
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
