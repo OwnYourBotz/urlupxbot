3c5585be05f328be79deb9bd59ea37f4c76ee789
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 16:37:12 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -24,7 +24,6 @@ else:
 
 # the Strings used for this "thing"
 from translation import Translation
-from database.database import *
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
