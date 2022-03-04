0edb0907ab85ca891aac748e719ccb1765910fa3
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 17:32:27 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -18,3 +18,21 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
+
+from Translation import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.database.add import add_user_to_database
+
+
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
+
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
