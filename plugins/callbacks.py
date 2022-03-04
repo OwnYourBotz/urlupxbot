9751120a307cbdd750f11084d9b26f7c9dcb3c03
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 17:38:35 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -86,3 +86,22 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         await db.set_thumbnail(cb.from_user.id, thumbnail=None)
         await cb.answer("Successfully Removed Custom Thumbnail!", show_alert=True)
         await cb.message.delete(True)
+
+async def OpenSettings(event: Message, user_id: int):
+    try:
+        await event.edit(
+            text="**⚙ Configure My Behaviour**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(f"🔰 Upload as {'File 🗃️' if ((await db.get_upload_as_doc(user_id)) is True) else 'Video 🎥'}",
+                                          callback_data="triggerUploadMode")],
+                    [InlineKeyboardButton("🌆 Custom Thumbnail ", callback_data="triggerThumbnail")],
+                    [InlineKeyboardButton("⛔ Close Settings", callback_data="close")]
+                ]
+            )
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await OpenSettings(event, user_id)
+    except MessageNotModified:
+        pass
