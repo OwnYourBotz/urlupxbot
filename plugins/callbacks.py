36f6bcc6c4d059ecb99c41d75395d58b0f609a2f
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-04 17:28:01 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -2,6 +2,11 @@
 
 import os
 
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
 
 
 from plugins.youtube_dl_button import youtube_dl_call_back
