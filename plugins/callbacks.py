b58244564094891584b734520f9e40719ced3f97
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-05 12:43:29 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -6,7 +6,7 @@ from sample_config import Config
 from translation import Translation
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from plugins.database.database import db
 
 @Client.on_callback_query()
 async def button(bot, update):
@@ -28,5 +28,48 @@ async def button(bot, update):
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
+    elif update.data == "OpenSettings":
+        await update.answer()
+        await show_settings(update.message)
+    elif update.data == "showThumbnail":
+        thumbnail = await db.get_thumbnail(update.from_user.id)
+        if not thumbnail:
+            await update.answer("You didn't set any custom thumbnail!", show_alert=True)
+        else:
+            await update.answer()
+            await bot.send_photo(update.message.chat.id, thumbnail, "Custom Thumbnail",
+                               reply_markup=types.InlineKeyboardMarkup([[
+                                   types.InlineKeyboardButton("Delete Thumbnail",
+                                                              callback_data="deleteThumbnail")
+                               ]]))
+    elif update.data == "deleteThumbnail":
+        await db.set_thumbnail(update.from_user.id, None)
+        await update.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
+        await update.message.delete(True)
+    elif update.data == "setThumbnail":
+        await update.answer()
+        await update.message.edit("Send me any photo to set that as custom thumbnail.\n\n"
+                              "Press /cancel to cancel process.")
+        from_user_thumb: "types.Message" = await bot.listen(update.message.chat.id)
+        if not from_user_thumb.photo:
+            await update.message.edit("Process Cancelled!")
+            return await from_user_thumb.continue_propagation()
+        else:
+            await db.set_thumbnail(cb.from_user.id, from_user_thumb.photo.file_id)
+            await update.message.edit("Okay!\n"
+                                  "Now I will apply this thumbnail to next uploads.",
+                                  reply_markup=types.InlineKeyboardMarkup(
+                                      [[types.InlineKeyboardButton("Show Settings",
+                                                                   callback_data="OpenSettings")]]
+                                  ))
+
+    elif update.data == "triggerUploadMode":
+        await update.answer()
+        upload_as_doc = await db.get_upload_as_doc(update.from_user.id)
+        if upload_as_doc:
+            await db.set_upload_as_doc(update.from_user.id, False)
+        else:
+            await db.set_upload_as_doc(update.from_user.id, True)
+        await show_settings(update.message)
     else:
         await update.message.delete()
