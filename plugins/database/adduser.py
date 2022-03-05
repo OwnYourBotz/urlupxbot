fe4a2613b643443fe4040f4c8bf4d6fbb24fa2da
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-05 09:09:59 +0530

    Update adduser.py

--- plugins/database/adduser.py
+++ plugins/database/adduser.py
@@ -1,7 +1,7 @@
 # (c) @AbirHasan2005
 
 from sample_config import Config
-from database.database.access_db import db
+from database.database.access import db
 from pyrogram import Client
 from pyrogram.types import Message
 
