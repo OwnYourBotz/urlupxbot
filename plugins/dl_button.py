32040dbd33fabc59f6dc60b2343bb53e0a1c4c9e
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-05 16:26:47 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -123,7 +123,7 @@ async def ddl_call_back(bot, update):
                 await bot.send_document(
                     chat_id=update.message.chat.id,
                     document=download_directory,
-                    thumb=thumbnail,
+                    thumb=thumb_image_path,
                     caption=description,
                     reply_to_message_id=update.message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
@@ -144,7 +144,7 @@ async def ddl_call_back(bot, update):
                     width=width,
                     height=height,
                     supports_streaming=True,
-                    thumb=thumbnail,
+                    thumb=thumb_image_path,
                     reply_to_message_id=update.message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
