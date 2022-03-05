c789f966a3107a9c4c1f769d394f842bba1c59d7
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-05 08:54:54 +0530

    Create settings.py

---
+++ plugins/settings/settings.py
@@ -0,0 +1,18 @@
+async def OpenSettings(event: Message, user_id: int):
+    try:
+        await event.edit(
+            text="**⚙ Configure My Behaviour**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(f"🔰 Upload as {'File 🗃️' if ((await db.get_upload_as_doc(user_id)) is True) else 'Video 🎥'}",
+                                          callback_data="triggerUploadMode")],
+                    [InlineKeyboardButton("🌆 Custom Thumbnail ", callback_data="triggerThumbnail")],
+                    [InlineKeyboardButton("⛔ Close Settings", callback_data="close")]
+                ]
+            )
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await OpenSettings(event, user_id)
+    except MessageNotModified:
+        pass
