0b859c468546797eebc935cfa29b93308ac65172
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-06 13:25:26 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -49,3 +49,11 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
     LOGGER = logging
+    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    # Update channel for Force Subscribe
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    
+    
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
+    PRO_USERS.append(OWNER_ID)
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
