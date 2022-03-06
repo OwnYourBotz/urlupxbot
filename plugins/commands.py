65f89d1bd25c8af901f8dfdae6ac31337659db2d
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-06 15:16:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -25,7 +25,7 @@ async def start(bot, update):
     await add_user_to_database(bot, update)
     await bot.send_message(
         Config.LOG_CHANNEL,
-           f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
     )
 
     if Config.UPDATES_CHANNEL:
