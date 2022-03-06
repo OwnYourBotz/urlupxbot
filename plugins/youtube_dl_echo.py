53496c38c5e4d0dfac8249c233e6baa890522db3
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-06 10:00:30 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -248,10 +248,33 @@ async def echo(bot, update):
                 )
             ])
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        await chk.delete(True)
+        # logger.info(reply_markup)
+        thumbnail = Config.DEF_THUMB_NAIL_VID_S
+        thumbnail_image = Config.DEF_THUMB_NAIL_VID_S
+        if "thumbnail" in response_json:
+            if response_json["thumbnail"] is not None:
+                thumbnail = response_json["thumbnail"]
+                thumbnail_image = response_json["thumbnail"]
+        thumb_image_path = DownLoadFile(
+            thumbnail_image,
+            Config.DOWNLOAD_LOCATION + "/" +
+            str(update.from_user.id) + f'{randem}' + ".webp",
+            Config.CHUNK_SIZE,
+            None,  # bot,
+            Translation.DOWNLOAD_START,
+            update.message_id,
+            update.chat.id
+        )
+        if os.path.exists(thumb_image_path):
+            im = Image.open(thumb_image_path).convert("RGB")
+            im.save(thumb_image_path.replace(".webp", ".jpg"), "jpeg")
+        else:
+            thumb_image_path = None
+        await chk.delete()
+        time.sleep(1)
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
+            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
