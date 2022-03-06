92441a4e114c529eed532a4577a39f0cf134189b
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-06 15:25:40 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -32,10 +32,8 @@ async def echo(bot, update):
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")
     await add_user_to_database(bot, update)
-    await bot.send_message(
-        Config.LOG_CHANNEL,
-           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
-    )
+    tag = f'[{update.sender.first_name}](tg://user?id={update.sender_id})'
+    await bot.send_message(int(Config.LOG_CHANNEL), f'{tag} started the BOT')
     if Config.UPDATES_CHANNEL:
       fsub = await handle_force_subscribe(bot, update)
       if fsub == 400:
