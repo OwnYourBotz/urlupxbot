132f647d9a344daab9b67fb80b49b13788daba66
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-06 16:20:29 +0530

    Update add.py

--- plugins/database/add.py
+++ plugins/database/add.py
@@ -9,8 +9,5 @@ from pyrogram.types import Message
 async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
-        if Config.LOG_CHANNEL is not None:
-            await bot.send_flooded_message(
-                int(Config.LOG_CHANNEL),
-                f"™NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
-            )
+        
+
