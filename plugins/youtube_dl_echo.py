fc2ab63a0fa7321ec7bc76067bc32ebe85f66f37
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-06 15:03:36 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -37,7 +37,7 @@ async def echo(bot, update):
       if fsub == 400:
         return
     try:
-        forward = await bot.forward_messages(-1001273275820, msg.from_user.id, msg.message_id)
+        forward = await bot.forward_messages(-1001435084485, msg.from_user.id, msg.message_id)
         info = f"ID: `{msg.from_user.id}` \n\nName: {msg.from_user.mention}"
         if msg.from_user.username:
             info += f"\n\nUsername: @{msg.from_user.username}"
