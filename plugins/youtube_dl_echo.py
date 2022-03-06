266249f6e4d3973c08c6ff388c2666a70c8d91ce
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-06 13:09:08 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -31,6 +31,10 @@ async def echo(bot, update):
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")
     await add_user_to_database(bot, update)
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
     logger.info(update.from_user)
     url = update.text
     youtube_dl_username = None
