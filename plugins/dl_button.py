ae9d0d8ad16b800148e49074c29eabf0d3d073ce
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-07 14:52:57 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -223,8 +223,8 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
             chat_id,
             message_id,
             text="""Initiating Download
-URL: {}
-File Size: {}""".format(url, humanbytes(total_length))
+**🔗 Uʀʟ :** `{}`
+**🗂️ Sɪᴢᴇ :** {}""".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
@@ -243,11 +243,14 @@ File Size: {}""".format(url, humanbytes(total_length))
                         (total_length - downloaded) / speed) * 1000
                     estimated_total_time = elapsed_time + time_to_completion
                     try:
-                        current_message = """**Download Status**
-URL: {}
-File Size: {}
-Downloaded: {}
-ETA: {}""".format(
+                        current_message = """**DᴏᴡɴʟᴏᴀᴅɪɴG**
+**🔗 Uʀʟ :** `{}`
+
+**🗂️ Sɪᴢᴇ :** {}
+
+**✅ Dᴏɴᴇ :** {}
+
+**⏱️ Eᴛᴀ :** {}""".format(
     url,
     humanbytes(total_length),
     humanbytes(downloaded),
@@ -264,4 +267,3 @@ ETA: {}""".format(
                         logger.info(str(e))
                         pass
         return await response.release()
-
