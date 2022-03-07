13a300066854c3f12640c58e6b734c86a2119539
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-07 12:31:34 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+from plugins.config import Config
+
+from pyrogram import Client as Tellybots
+from pyrogram import filters
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+if __name__ == "__main__" :
+    # create download directory, if not exist
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(root="plugins")
+    Tellybots = Tellybots(
+        "Uploader Bot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins)
+    Tellybots.run()
