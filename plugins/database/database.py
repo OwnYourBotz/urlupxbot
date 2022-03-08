0c2d35fa7dd2d591e3779b13f35e0a15e53e002c
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-08 15:22:24 +0530

    Update database.py

--- plugins/database/database.py
+++ plugins/database/database.py
@@ -4,8 +4,8 @@ import datetime
 import motor.motor_asyncio
 from plugins.config import Config
 
-
 class Database:
+
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
@@ -15,10 +15,10 @@ class Database:
         return dict(
             id=id,
             join_date=datetime.date.today().isoformat(),
-            apply_caption=True,
             upload_as_doc=False,
             thumbnail=None,
-            caption=None
+            generate_ss=False,
+            generate_sample_video=False
         )
 
     async def add_user(self, id):
@@ -27,24 +27,18 @@ class Database:
 
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return bool(user)
+        return True if user else False
 
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
 
     async def get_all_users(self):
-        return self.col.find({})
+        all_users = self.col.find({})
+        return all_users
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
-
-    async def set_apply_caption(self, id, apply_caption):
-        await self.col.update_one({'id': id}, {'$set': {'apply_caption': apply_caption}})
-
-    async def get_apply_caption(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('apply_caption', True)
 
     async def set_upload_as_doc(self, id, upload_as_doc):
         await self.col.update_one({'id': id}, {'$set': {'upload_as_doc': upload_as_doc}})
@@ -60,12 +54,19 @@ class Database:
         user = await self.col.find_one({'id': int(id)})
         return user.get('thumbnail', None)
 
-    async def set_caption(self, id, caption):
-        await self.col.update_one({'id': id}, {'$set': {'caption': caption}})
+    async def set_generate_ss(self, id, generate_ss):
+        await self.col.update_one({'id': id}, {'$set': {'generate_ss': generate_ss}})
 
-    async def get_caption(self, id):
+    async def get_generate_ss(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user.get('caption', None)
+        return user.get('generate_ss', False)
+
+    async def set_generate_sample_video(self, id, generate_sample_video):
+        await self.col.update_one({'id': id}, {'$set': {'generate_sample_video': generate_sample_video}})
+
+    async def get_generate_sample_video(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('generate_sample_video', False)
 
 
     async def get_user_data(self, id) -> dict:
