ee5a24f78058dd76325a10d22f79e45a8dd53ee9
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-09 20:50:26 +0530

    Update settings.py

--- plugins/settings/settings.py
+++ plugins/settings/settings.py
@@ -22,7 +22,7 @@ async def OpenSettings(m: "types.Message"):
                                     callback_data="triggerUploadMode")],
         #[types.InlineKeyboardButton(f"Generate Sample Video {'✅' if generate_sample_video else '❌'}", 
                                     #callback_data="triggerGenSample")],
-        [types.InlineKeyboardButton(f"Generate Screenshots {'✅' if generate_ss else '❌'}", 
+        [types.InlineKeyboardButton(f"ɢᴇɴᴇʀᴀᴛᴇ sᴄʀᴇᴇɴsʜᴏᴛs {'✅' if generate_ss else '❌'}", 
                                     callback_data="triggerGenSS")],
         [types.InlineKeyboardButton(f"{'ᴄʜᴀɴɢᴇ' if thumbnail else '🌃 sᴇᴛ'} ᴛʜᴜᴍʙɴᴀɪʟ",
                                     callback_data="setThumbnail")]
