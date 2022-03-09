50ed6d3d708e0d0c63f10f0dc667930a7c907bd1
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-09 10:31:51 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -285,6 +285,37 @@ async def youtube_dl_call_back(bot, update):
                 logger.info("Did this happen? :\\")
             end_two = datetime.now()
             time_taken_for_upload = (end_two - end_one).seconds
+            if (await db.get_generate_ss(update.from_user.id)) is True:
+            media_album_p = []
+            if images is not None:
+                i = 0
+                caption = "© @TGBotsZ"
+                if is_w_f:
+                    caption = "/upgrade to Plan D to remove the watermark\n© @AnyDLBot"
+                for image in images:
+                    if os.path.exists(str(image)):
+                        if i == 0:
+                            media_album_p.append(
+                                InputMediaPhoto(
+                                    media=image,
+                                    caption=caption,
+                                    parse_mode="html"
+                                )
+                            )
+                        else:
+                            media_album_p.append(
+                                InputMediaPhoto(
+                                    media=image
+                                )
+                            )
+                        i = i + 1
+            await bot.send_media_group(
+                chat_id=update.message.chat.id,
+                disable_notification=True,
+                reply_to_message_id=update.message.message_id,
+                media=media_album_p
+            )
+            #
             try:
                 shutil.rmtree(tmp_directory_for_each_user)
                 os.remove(thumbnail)
